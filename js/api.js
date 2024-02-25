93a53df8592e2d2f106c39b3d235ed31ae9d4d62
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-02-25 19:09:34 +0100

    25.02

---
+++ js/api.js
@@ -0,0 +1,1 @@
+const api = "https://v2.api.noroff.dev/rainy-days";
---
+++ js/utils.js
@@ -0,0 +1,14 @@
+export function listData(list, out){
+    //console.log ("List:", list);
+    out.innerHTML = "";
+    let newDivs = "";
+    for (let card of list) {
+        //console.log(card);
+        newDivs += `<div>
+        <img src="${card.image}" alt="${card.name}">
+        <h2>${card.name}</h2>
+        <p>From: ${card.amiiboSeries}</p>
+        <a href="./rainy-days.html?id=${card.head}${card.tail}">Read More</a>
+        </div>`;
+    }
+    out.innerHTML = newDivs;
